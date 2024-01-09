8d47d6555890bc0f8362b28dc4ffaaf28d854953
Naelerd <juampi_22_55@hotmail.com>
2024-01-09 14:32:40 -0300

    Commit 1.0

---
+++ src/main/java/com/portafolio/portafolioWEB/PortafolioWebApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.portafolioWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioWebApplication.class, args);
+	}
+
+}
