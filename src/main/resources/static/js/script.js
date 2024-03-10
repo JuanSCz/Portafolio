2d16ac36ad47b44db5dc07c7116c1e95e3e08571
Naelerd <juampi_22_55@hotmail.com>
2024-03-09 23:47:04 -0300

    Commit 4.0

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -49,3 +49,85 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var certificados = [
+        ["images/FullStackEgg.png", "images/CertificadoSpring.png"]/*,
+        ["images/FullStackEgg.png", "images/CertificadoSpring.png"] */
+    ];
+
+    var contenedorFlotante = crearContenedorFlotante("contenedorFlotante", certificados);
+
+  /*  var certificadoButtonUno = document.getElementById("buttonCertificadoUno"); */
+    var certificadoButtonDos = document.getElementById("buttonCertificadoDos");
+
+ /*   certificadoButtonUno.addEventListener("click", function (e) {
+        e.preventDefault();
+        mostrarContenedorFlotante(contenedorFlotante, certificados[0]);
+    }); */
+
+    certificadoButtonDos.addEventListener("click", function (e) {
+        e.preventDefault();
+        mostrarContenedorFlotante(contenedorFlotante, certificados[0]);
+    });
+});
+
+function crearContenedorFlotante(id, imagenes) {
+    var contenedorFlotante = document.createElement("div");
+    contenedorFlotante.id = id;
+    contenedorFlotante.className = "contenedorFlotante";
+    contenedorFlotante.style.display = "none";
+    contenedorFlotante.style.position = "fixed";
+    contenedorFlotante.style.top = "0";
+    contenedorFlotante.style.left = "0";
+    contenedorFlotante.style.width = "100%";
+    contenedorFlotante.style.height = "100%";
+    contenedorFlotante.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
+
+    var row = document.createElement("div");
+    row.className = "row";
+    row.id = "certificados";
+
+    var imagenesContainer = document.createElement("div");
+    imagenesContainer.className = "imagenesContainer";
+
+    for (var i = 0; i < imagenes.length; i++) {
+        var img = document.createElement("img");
+        img.className = "col-lg-6 imageCertificados";
+        img.src = imagenes[i];
+        img.alt = "Certificado " + (i + 1);
+        imagenesContainer.appendChild(img);
+    }
+
+    row.appendChild(imagenesContainer);
+    contenedorFlotante.appendChild(row);
+
+    var salirButton = document.createElement("button");
+    salirButton.innerText = "Salir";
+    salirButton.className = "salirButton";
+    salirButton.addEventListener("click", function () {
+        contenedorFlotante.style.display = "none";
+    });
+
+    contenedorFlotante.appendChild(salirButton);
+
+    document.body.appendChild(contenedorFlotante);
+
+    return contenedorFlotante;
+}
+
+function mostrarContenedorFlotante(contenedor, imagenes) {
+    var imagenesContainer = contenedor.querySelector(".imagenesContainer");
+    imagenesContainer.innerHTML = "";
+
+    for (var i = 0; i < imagenes.length; i++) {
+        var img = document.createElement("img");
+        img.className = "col-lg-6 imageCertificados";
+        img.src = imagenes[i];
+        img.alt = "Certificado " + (i + 1);
+        imagenesContainer.appendChild(img);
+    }
+
+    contenedor.style.display = "flex";
+}
